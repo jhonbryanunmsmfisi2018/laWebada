1841c2546bf21608c38448c226076dba311028e4
jhonbryanunmsmfisi2018 <jhon.gonzalez@unmsm.edu.pe>
2021-12-31 16:44:06 -0500

    Arreglar formularios y validaciones

--- src/keys.js
+++ src/keys.js
@@ -1,17 +1,17 @@
-/*module.exports = {
+module.exports = {
    database: {
       host: 'us-cdbr-east-05.cleardb.net',
       user: 'b7fd6191524734',
       password: '9992cba2',
       database: 'heroku_a2fd79052888e7b',
    },
-};*/
+};
 
- module.exports = {
+ /*module.exports = {
    database: {
    host: '127.0.0.1',
    user: 'root',
    password: 'root',
    database: 'lawebada',
    },
-};
+};*/
